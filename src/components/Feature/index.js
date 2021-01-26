6f1a304c69d71d921ceff9cea920be2b853799b7
Fabiola Benitez <lumediaweb@gmail.com>
2021-01-25 21:23:20 -0300

    Cambios en feature index y setuptest

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -4,7 +4,7 @@ import { FeatureContainer, FeatureButton } from './Feature';
 const Feature = () => {
   return (
     <FeatureContainer>
-      <h1>Pizza of the Day</h1>
+      <h1>Menu of the Day</h1>
       <p>Truffle alfredo sauce topped with 24 carat gold dust</p>
       <FeatureButton>Order Now</FeatureButton>
     </FeatureContainer>
